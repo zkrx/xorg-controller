c2df9b7b177cc01d328e78e7b1e6b411b54ac025
zkrx <zk_rex@hotmail.com>
2022-09-19 17:38:25 +0200

    ballmouse: adapt to nintendo switch pro controller
    Also tidy up coding style here and there.

--- BallMouse.c
+++ BallMouse.c
@@ -20,29 +20,24 @@ static inline void event(int ofd, int type, int code, int val) {
 	out.type = type;
 	out.code = code;
 	out.value = val;
-	/*Do some simple remapping to be more mouse-like*/
+	/* Do some simple remapping to be more mouse-like */
 	if (out.type == EV_KEY) {
-		if (out.code == BTN_7)		out.code = BTN_LEFT;
-		if (out.code == BTN_1)		out.code = BTN_MIDDLE;
-		if (out.code == BTN_2)		out.code = BTN_RIGHT;
-		if (out.code == BTN_C)		out.code = BTN_1;
-		if (out.code == BTN_EAST)	out.code = BTN_2;
-		if (out.code == BTN_SOUTH)	out.code = BTN_7;
+		if (out.code == BTN_NORTH)	out.code = BTN_MIDDLE;
+		if (out.code == BTN_EAST)	out.code = BTN_RIGHT;
+		if (out.code == BTN_SOUTH)	out.code = BTN_LEFT;
 	}
 	if (out.type == EV_REL) {
-		/* Seems like we need to invert Y for right-hand operation. */
-		if (out.code == REL_Y) out.value = -out.value;
 		if (out.code == REL_RZ) {
 			out.code = REL_WHEEL;
 			out.value = out.value*WVEL;
 		}
 	}
 
-	write(ofd,&out,sizeof(out));
+	write(ofd, &out, sizeof(out));
 }
 
 int main(int argc, char **argv) {
-	struct libevdev *spaceball = NULL;
+	struct libevdev *controller;
 	struct input_event in, out;
 	int ifd;
 	int ofd;
@@ -64,66 +59,69 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
+	/* Find controller containing IN_DEVNAME in its name */
 	while ((evfile = readdir(evdir)) != NULL) {
-		ifd = open(evfile->d_name,O_RDONLY|O_NONBLOCK);
-		code = libevdev_new_from_fd(ifd,&spaceball);
-		if (code < 0) { //Can't create evdev instance.
+		ifd = open(evfile->d_name, O_RDONLY|O_NONBLOCK);
+
+		code = libevdev_new_from_fd(ifd, &controller);
+		if (code < 0) {
 			close(ifd);
 			continue;
 		}
-		/* A real spaceball -- rather than another BallMouse
-		 * will have EV_ABS events. */
-		if(strstr(libevdev_get_name(spaceball),"SpaceBall") && libevdev_has_event_type(spaceball,EV_ABS)) {
-			printf("Attaching to device: %s\n",libevdev_get_name(spaceball));
-			break;	//This is it.  We just do the first one
+
+		/* TODO: strncmp() */
+		if(!strcmp(libevdev_get_name(controller), IN_DEVNAME)) {
+			printf("Attaching to device: %s\n",libevdev_get_name(controller));
+			break;
 		}
+
 		/* Otherwise free resources up for the next iteration. */
-		libevdev_free(spaceball);
-		spaceball = NULL;
+		libevdev_free(controller);
+		controller = NULL;
 		close(ifd);
 	}
 
-	if (spaceball == NULL) {
+	if (controller == NULL) {
 		printf("Can't find appropriate device.\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	/* Set up monitoring for input data */
 	inpoll[0].fd = ifd;
-	inpoll[0].events = POLLIN||POLLRDBAND;
+	inpoll[0].events = POLLIN || POLLRDBAND;
 
 	/* Now set up the output. */
-	ofd = open("/dev/uinput",O_WRONLY|O_NONBLOCK);
-	memset(&mouse,0,sizeof(mouse)); //Probably not actually required.
+	ofd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+	memset(&mouse, 0, sizeof(mouse)); /* Probably not actually required. */
 	
-	//Describe the fake mouse
+	/* Describe the fake mouse */
 	mouse.id.bustype	= BUS_USB;
-	mouse.id.vendor		= 0x05A8;	//Just use the one for Spacetec
+	mouse.id.vendor		= 0x05A8;	/* Just use the one for Spacetec */
 	mouse.id.product	= 0xFFE0;
-	strcpy(mouse.name,DEVNAME);
+	strcpy(mouse.name, OUT_DEVNAME);
 
-	ioctl(ofd,UI_SET_EVBIT,EV_REL);
-	ioctl(ofd,UI_SET_RELBIT,REL_X);
-	ioctl(ofd,UI_SET_RELBIT,REL_Y);
-	ioctl(ofd,UI_SET_RELBIT,REL_Z);
-	ioctl(ofd,UI_SET_RELBIT,REL_WHEEL);
-	ioctl(ofd,UI_SET_RELBIT,REL_HWHEEL);
-	ioctl(ofd,UI_SET_RELBIT,REL_RX);
-	ioctl(ofd,UI_SET_RELBIT,REL_RY);
-	ioctl(ofd,UI_SET_RELBIT,REL_RZ);
-	ioctl(ofd,UI_SET_EVBIT,EV_KEY);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_1);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_LEFT);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_2);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_3);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_4);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_5);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_6);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_RIGHT);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_7);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_8);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_9);
-	ioctl(ofd,UI_SET_KEYBIT,BTN_MIDDLE);
+	ioctl(ofd, UI_SET_EVBIT, EV_REL);
+	ioctl(ofd, UI_SET_RELBIT, REL_X);
+	ioctl(ofd, UI_SET_RELBIT, REL_Y);
+	ioctl(ofd, UI_SET_RELBIT, REL_Z);
+	ioctl(ofd, UI_SET_RELBIT, REL_WHEEL);
+	ioctl(ofd, UI_SET_RELBIT, REL_HWHEEL);
+	ioctl(ofd, UI_SET_RELBIT, REL_RX);
+	ioctl(ofd, UI_SET_RELBIT, REL_RY);
+	ioctl(ofd, UI_SET_RELBIT, REL_RZ);
+	ioctl(ofd, UI_SET_EVBIT, EV_KEY);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_1);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_LEFT);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_2);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_3);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_4);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_5);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_6);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_RIGHT);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_7);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_8);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_9);
+	ioctl(ofd, UI_SET_KEYBIT, BTN_MIDDLE);
 
 /*	These buttons appear in the actual device, but if we report them here, MatchIsMouse doesn't match. */
 //	ioctl(ofd,UI_SET_KEYBIT,BTN_C);
@@ -132,8 +130,8 @@ int main(int argc, char **argv) {
 /*	... so replace them with other buttons.	*/
 
 
-	ioctl(ofd,UI_DEV_SETUP,&mouse);
-	code = ioctl(ofd,UI_DEV_CREATE);
+	ioctl(ofd, UI_DEV_SETUP, &mouse);
+	code = ioctl(ofd, UI_DEV_CREATE);
 	if (code) {
 		printf("Can't create output device.\n");
 		exit(1);
@@ -141,30 +139,40 @@ int main(int argc, char **argv) {
 
 
 	/* Events are processed here. */
-
 	code = 0;
-	while((code == 0)||(code == 1)||(code == -EAGAIN)) {
-		code = libevdev_next_event(spaceball,LIBEVDEV_READ_FLAG_NORMAL,&in);
+	while (code == 0 || code == 1 || code == -EAGAIN) {
+		code = libevdev_next_event(controller, LIBEVDEV_READ_FLAG_NORMAL, &in);
 		if (code == -EAGAIN) {
-			while (poll(inpoll,1,500) == 0); //0 is a timeout, so we just go again.
+			while (poll(inpoll, 1, 500) == 0); /* 0 is a timeout, so we just go again. */
 			continue;
 		}
-		if (code ==0) { /*This is a real event */
-			/* We just kind of fake this one by sending a clean SYN event for each one we get. */
+
+		if (code == 0) { /* This is a real event */
+			/* We just kind of fake this one by sending
+			 * a clean SYN event for each one we get.
+			 */
 			if (in.type == EV_SYN) {
 				if((in.code == SYN_REPORT) && (in.value == 0)) {
-					event(ofd,EV_SYN,SYN_REPORT,0);	
+					event(ofd, EV_SYN, SYN_REPORT,0);	
 				}
 				continue;
 			}
-			if (in.type == EV_ABS) {//Handle absolute motion.
-				if (abs(in.value) > ABS_MIN) {	/* Allow a bit of a dead spot near the center. */
-					event(ofd,EV_REL,in.code,in.value*VELOCITY);
+
+			/* FIXME: this is a hack, and this is usable. But it is jerky.
+			 * For starter, we should repeat the last events unless it is zero
+			 * (or inside the dead zone, see ABS_MIN). Otherwise we need to
+			 * perpetually move the stick or the cursor stops.
+			 */
+			if (in.type == EV_ABS) { /* Handle absolute motion. */
+//				printf("in.value: %d; %d\n", in.value, (int)((float)in.value*VELOCITY));
+				if (abs(in.value) > ABS_MIN) {
+					/* Allow a bit of a dead spot near the center. */
+					event(ofd, EV_REL, in.code, in.value*VELOCITY);
 					continue;
 				}
 			}
-			if (in.type == EV_KEY) {//Handle button events.
-				event(ofd,in.type,in.code,in.value);
+			if (in.type == EV_KEY) { /* Handle button events. */
+				event(ofd, in.type, in.code, in.value);
 				continue;
 			}
 		}
--- config.h
+++ config.h
@@ -1,22 +1,29 @@
 #ifndef BALLMOUSE_CONFIG_H
 #define BALLMOUSE_CONFIG_H
 
-/* Minimum value that we must produce to register a movement.  This is not adjusted for velocity and
- * 	can be quite large */
-#define ABS_MIN		500
+/* Minimum value that we must produce to register a
+ * movement. This is not adjusted for velocity and
+ * can be quite large.
+ */
+#define ABS_MIN		0
 
-/* ... and this is the adjustment we make for mouse velocity on the display, since the native coordinate
- * 	system is quite large. */
-#define VELOCITY	.009
+/* ... and this is the adjustment we make for mouse
+ * velocity on the display, since the native coordinate
+ * system is quite large.
+ */
+#define VELOCITY	.0009
 
 /* Farther scaling to apply to the wheel velocity later. */
 #define WVEL		.2
 
-/* Because the input subsystem needs to know, we'll cap our relative motion at this number. */
+/* Because the input subsystem needs to know, we'll cap
+ * our relative motion at this number.
+ */
 #define	MAXVAL		255
 
 /* What will we call our output device? */
-#define	DEVNAME		"BallMouse SpaceBall Proxy Server"
+#define IN_DEVNAME		"Nintendo Switch Pro Controller"
+#define	OUT_DEVNAME		"Nintendo Switch Pro Remapped"
 
 
 #endif
